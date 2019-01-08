35f27bcfe4043b6f2ddb6ab7c174a0a7867a0b17
maojiawei2012180@126.com <maojiawei2012180@126.com>
2019-01-08 16:45:07 +0800

    优化算法

--- src/main/java/io/jovi/gyaradoseu/algorithm/primary/array/DuplicatesRemove.java
+++ src/main/java/io/jovi/gyaradoseu/algorithm/primary/array/DuplicatesRemove.java
@@ -24,12 +24,14 @@ public class DuplicatesRemove {
      * @return
      */
     public static int removeDuplicates(int[] nums) {
+        if(nums.length==0) {
+            return 1;
+        }
         int index = 1;
         for(int i=1;i<nums.length;i++){
 
             if(nums[i]!=nums[i-1]) {
-                nums[index]=nums[i];
-                index++;
+                nums[++index]=nums[i];
             }
         }
         return index;
