34adad73513f1c32714b71c49ed38e7e8d8c1ba7
maojiawei2012180@126.com <maojiawei2012180@126.com>
2019-12-05 22:23:14 +0800

    基于LinkedHashMap的LRU算法

---
+++ src/main/java/io/jovi/gyaradoseu/algorithm/lru/LRUCache.java
@@ -0,0 +1,54 @@
+package io.jovi.gyaradoseu.algorithm.lru;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * Title:
+ * </p >
+ * <p>
+ * Description:
+ * </p >
+ * <p>
+ * Copyright: Copyright (c) 2019
+ * All rights reserved. 2019-12-05.
+ * </p >
+ *
+ * @author MaoJiaWei
+ * @version 1.0
+ */
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private int size;
+
+    public static void main(String[] args) {
+        LRUCache<Integer, Integer> cache = LRUCache.newInstance(2);
+        cache.put(1, 1);
+        System.out.println("1========================================");
+        System.out.println(cache);
+        cache.put(2, 2);
+        System.out.println("2========================================");
+        System.out.println(cache);
+        cache.put(1, 1);
+        System.out.println("3========================================");
+        System.out.println(cache);
+        cache.put(3, 3);
+        System.out.println("4========================================");
+        System.out.println(cache);
+    }
+
+    private LRUCache(int size) {
+        super(size, 0.75f, true);
+        this.size = size;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > size;
+    }
+
+    public static <K, V> LRUCache<K, V> newInstance(int size) {
+        return new LRUCache<K, V>(size);
+    }
+
+}
