d8f10ba056042a32cc374425849cca733c2256fb
maojiawei2012180@126.com <maojiawei2012180@126.com>
2019-12-08 14:30:31 +0800

    快速排序

---
+++ src/main/java/io/jovi/gyaradoseu/algorithm/sort/quick/QuickSort.java
@@ -0,0 +1,42 @@
+package io.jovi.gyaradoseu.algorithm.sort.quick;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2019
+ * All rights reserved. 2019-12-08.
+ * </p>
+ *
+ * @author MaoJiaWei
+ * @version 1.0
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = { 5,2,4,9,7 };
+    }
+
+    /**
+     * 快速排序
+     * @param nums
+     * @return
+     */
+    public static int[] split(int[] nums){
+        return nums;
+    }
+
+    /**
+     *
+     * @param nums
+     * @param start
+     * @param end
+     */
+    private static void sort(int[] nums,int start,int end){
+
+    }
+
+}
