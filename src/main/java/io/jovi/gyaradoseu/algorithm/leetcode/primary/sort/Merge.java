848552e303c6d963aaea787604efcca7927fd1b8
jovimao <maojiawei2012180@126.com>
2019-12-11 09:50:18 +0800

    合并两个有序数组

--- src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/primary/sort/Merge.java
+++ src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/primary/sort/Merge.java
@@ -34,23 +34,38 @@ public class Merge {
         int m = 3;
         int[] num2 = new int[]{2, 5, 6};
         int n = 3;
-        merge(num1,m,num2,3);
+        merge(num1,m,num2,n);
         for (int num : num1) {
             System.out.println(num);
         }
 
     }
 
+    /**
+     * 合并两个有序数组
+     * 由于两个数组都是有序的，从前往后排比较困难 因为涉及后移O(n)。
+     * 既然都是有序的，并且知道每个数组对应的长度，我们就可以从后往前插入。
+     * 一个一个遍历，将大的数据插入至最后
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int length = m;
-        // x作为nums1的指针 y作为nums2的指针
-        int x = 0 , y = 0;
-        for(; x<m; x++){
-            if(nums1[x] < nums2[y]){
-                y++;
+        // i 代表 nums1 数组的结尾指针
+        // j 代表 nums2 数组的结尾指针
+        // k 代表 合并后数组的结尾指针
+        int i = m-1,j = n-1,k = m+n-1;
+        while (i > 0 && j > 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
                 break;
             }
-
+            nums1[k--] = nums2[j--];
         }
+        while(j>=0){
+            nums1[k--] = nums2[j--];
+        }
+
     }
 }
