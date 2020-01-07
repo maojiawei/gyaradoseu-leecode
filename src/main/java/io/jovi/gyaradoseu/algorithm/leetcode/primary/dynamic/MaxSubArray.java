e1413e94af24d0b307b09d24a79fa259707e6b82
jovimao <maojiawei2012180@126.com>
2020-01-07 13:14:27 +0800

    最大子序和

--- src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/primary/dynamic/MaxSubArray.java
+++ src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/primary/dynamic/MaxSubArray.java
@@ -25,4 +25,57 @@ package io.jovi.gyaradoseu.algorithm.leetcode.primary.dynamic;
  * @version 1.0
  */
 public class MaxSubArray {
+    /**
+     * 最大子序和
+     * 暴力穷举法O(N的平方)
+     * @param nums
+     * @
+     */
+    public static int maxSubArray(int[] nums) {
+        // 当数组只有一个元素的时候，数组最大子序和就是他第一个元素
+        if(nums.length == 1){
+            return nums[0];
+        }
+        // 最大的sum值
+        int maxsum = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            // 不断累积
+            int sum = nums[i];
+            if(sum > maxsum){
+                maxsum = sum;
+            }
+            for(int j=i+1;j<nums.length;j++){
+                sum += nums[j];
+                if(sum > maxsum){
+                    maxsum = sum;
+                }
+            }
+        }
+        return maxsum;
+    }
+
+    /**
+     * 动态规划
+     * @param nums
+     * @return
+     */
+    public int maxSubArray2(int[] nums) {
+        // 当前的集合
+        int prev=nums[0];
+        // 最大值
+        int max=prev;
+        for(int i=1;i<nums.length;i++){
+            // prev+nums[i] < nums[i] 说明的当前的累加对后面的累加没有增益效果
+            // 如果pre=-100 nums[i]=300 那么-100只会对后面的累积拖后腿 所有直接舍弃-100 算作300
+            prev=Math.max(prev+nums[i],nums[i]);
+            max=Math.max(prev,max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,1};
+        int maxsum = maxSubArray(nums);
+        System.out.println(maxsum);
+    }
 }
