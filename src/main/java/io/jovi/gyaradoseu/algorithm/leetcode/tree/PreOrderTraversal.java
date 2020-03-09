185b86fab9ee43179e4aaf72b561ff4ef3147191
maojiawei2012180@126.com <maojiawei2012180@126.com>
2020-03-09 21:31:08 +0800

    前序遍历

--- src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/tree/PreOrderTraversal.java
+++ src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/tree/PreOrderTraversal.java
@@ -3,6 +3,7 @@ package io.jovi.gyaradoseu.algorithm.leetcode.tree;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <p>
@@ -18,28 +19,56 @@ import java.util.List;
  * @author Jovi
  * @version 1.0
  */
-public class PreorderTraversal {
+public class PreOrderTraversal {
     /**
      * 前序遍历(递归)
      * 根左右
      * @param root
      * @return
      */
-    public static List<Integer> preorderTraversal(TreeNode root){
+    public static List<Integer> preOrderTraversal(TreeNode root){
         List<Integer> list = new LinkedList<>();
         // 1. 遍历根节点
         list.add(root.getVal());
         // 2. 遍历左节点
         if(root.getLeft() != null){
-            list.addAll(preorderTraversal(root.getLeft()));
+            list.addAll(preOrderTraversal(root.getLeft()));
         }
         // 3. 遍历右节点
         if(root.getRight() != null){
-            list.addAll(preorderTraversal(root.getRight()));
+            list.addAll(preOrderTraversal(root.getRight()));
         }
 
         return list;
     }
+
+    /**
+     * 前序遍历(迭代)
+     * 根左右
+     * @param root
+     * @return
+     */
+    public static List<Integer> preOrderTraversal2(TreeNode root){
+        List<Integer> list = new LinkedList<>();
+        // 栈 先进后出 利用该特点 预存左右子树信息
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        //循环遍历 直至栈为空停止
+        while (!stack.empty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            // 先存入右子数 因为需要先取出左子树（先进后出）
+            if(node.right != null){
+                stack.push(node.right);
+            }
+            // 先存入左子树
+            if(node.left != null){
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
@@ -60,7 +89,7 @@ public class PreorderTraversal {
         TreeNode node1122 = new TreeNode(7);
         node112.right = node1122;
 
-        List<Integer> list = preorderTraversal(root);
+        List<Integer> list = preOrderTraversal2(root);
         System.out.println(list);
     }
 }
