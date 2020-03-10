a085c412b49ac4b72270c96e0dfb7ae11be0b3dc
jovimao <maojiawei2012180@126.com>
2020-03-10 17:07:23 +0800

    广度遍历优先

--- src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/tree/BreadFirstSearch.java
+++ src/main/java/io/jovi/gyaradoseu/algorithm/leetcode/tree/BreadFirstSearch.java
@@ -46,6 +46,7 @@ public class BreadFirstSearch {
         }
         return list;
     }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         TreeNode node1 = new TreeNode(2);
